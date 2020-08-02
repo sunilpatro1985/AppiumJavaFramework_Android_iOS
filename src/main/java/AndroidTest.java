1362be27a05bfed58c8f2e6f89d9c7fad7124dae
sunilpatro1985 <sunilpatro1985@gmail.com>
2020-08-02 13:38:38 +0530

    scroll using swipe & mobile:scroll

--- src/main/java/AndroidTest.java
+++ src/main/java/AndroidTest.java
@@ -1,4 +1,7 @@
+import base.AppDriver;
+import base.Util;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
@@ -7,6 +10,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 import java.util.List;
 
 public class AndroidTest {
@@ -22,20 +26,35 @@ public class AndroidTest {
         cap.setCapability("automationName", "UiAutomator2");
         //cap.setCapability("appPackage", "com.android.calculator2");
         //cap.setCapability("appActivity", ".Calculator");
-        cap.setCapability("appPackage", "com.google.android.dialer");
-        cap.setCapability("appActivity", ".DialtactsActivity");
+        //cap.setCapability("appPackage", "com.google.android.dialer");
+        //cap.setCapability("appActivity", ".DialtactsActivity");
+        cap.setCapability("appPackage", "com.swaglabsmobileapp");
+        cap.setCapability("appActivity", ".MainActivity");
 
         driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
+        AppDriver.setDriver(driver);
         Thread.sleep(1000);
 
-        driver.findElement(By.xpath("//android.widget.ImageView[@content-desc='Contacts tab.']")).click();
+        driver.findElement(MobileBy.AccessibilityId("test-Username")).sendKeys("standard_user");
+        driver.findElement(MobileBy.AccessibilityId("test-Password")).sendKeys("secret_sauce");
+        driver.findElement(MobileBy.AccessibilityId("test-LOGIN")).click();
+
+        /*driver.findElement(By.xpath("//android.widget.ImageView[@content-desc='Contacts tab.']")).click();
         Thread.sleep(500);
         List<MobileElement> contacts = driver.findElements(By.id("com.google.android.dialer:id/contact_name"));
         contacts.get(0).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("//android.widget.EditText[@text='First name']")).sendKeys("Welcome");
-        driver.findElement(By.id("com.android.contacts:id/editor_menu_save_button")).click();
-        Thread.sleep(3000);
+        driver.findElement(By.id("com.android.contacts:id/editor_menu_save_button")).click();*/
+
+        Thread.sleep(2000);
+
+        //Util.scrollTo("$7.99");
+
+        Util.scroll(Util.ScrollDirection.DOWN, 0.7);
+
+        Thread.sleep(2000);
+
         driver.quit();
     }
 }
--- src/main/java/IOSTest.java
+++ src/main/java/IOSTest.java
@@ -1,3 +1,5 @@
+import base.AppDriver;
+import base.Util;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.ios.IOSDriver;
@@ -20,18 +22,30 @@ public class IOSTest {
         cap.setCapability("automationName", "XCUITest");
         cap.setCapability("platformVersion", "13.5");
         //cap.setCapability("bundleId", "com.SamadiPour.SimpleCalculator");
-        cap.setCapability("bundleId", "com.example.apple-samplecode.UICatalog");
+        //cap.setCapability("bundleId", "com.example.apple-samplecode.UICatalog");
+        cap.setCapability("bundleId", "org.reactjs.native.example.SwagLabsMobileApp");
+
 
         driver = new IOSDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), cap);
-        Thread.sleep(1000);
+        AppDriver.setDriver(driver);
 
-        driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name='Action Sheets']")).click();
+        Thread.sleep(1000);
+        driver.findElement(By.id("test-Username")).sendKeys("standard_user");
+        driver.findElement(By.id("test-Password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("test-LOGIN")).click();
+
+        /*driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name='Action Sheets']")).click();
         driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name='Okay / Cancel']")).click();
         driver.findElement(By.xpath("//XCUIElementTypeButton[@name='OK']")).click();
-        driver.findElement(By.xpath("//XCUIElementTypeButton[@name='UICatalog']")).click();
+        driver.findElement(By.xpath("//XCUIElementTypeButton[@name='UICatalog']")).click();*/
 
-        Thread.sleep(3000);
+        Thread.sleep(2000);
 
+        //Util.scrollTo("$7.99");
+        Util.scroll(Util.ScrollDirection.DOWN, 0.7);
+
+
+        Thread.sleep(2000);
         driver.quit();
     }
 }
