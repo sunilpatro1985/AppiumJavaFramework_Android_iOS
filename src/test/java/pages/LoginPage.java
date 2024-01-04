9ccc0c017d919d19dad2d56fa19a5e31df93de1c
sunilpatro1985 <sunilpatro1985@gmail.com>
2024-01-04 17:52:09 +0530

    invalid login tests

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -9,15 +9,24 @@ import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
-public class LoginPage extends BasePage{
+public class LoginPage extends BasePage {
 
     private By userName;
+    private By userNameErrorText;
+    private By passwordErrorText;
+    private By credentialsErrorText;
 
-    public LoginPage(){
-        if(AppDriver.getCurrentDriver() instanceof AndroidDriver){
+    public LoginPage() {
+        if (AppDriver.getCurrentDriver() instanceof AndroidDriver) {
             userName = AppiumBy.accessibilityId("Username input field");
-        }else if(AppDriver.getCurrentDriver() instanceof IOSDriver){
+            userNameErrorText = By.xpath("//android.view.ViewGroup[@content-desc='Username-error-message']/android.widget.TextView");
+            passwordErrorText = By.xpath("//android.view.ViewGroup[@content-desc='Password-error-message']/android.widget.TextView");
+            credentialsErrorText = By.xpath("//android.view.ViewGroup[@content-desc='generic-error-message']/android.widget.TextView");
+        } else if (AppDriver.getCurrentDriver() instanceof IOSDriver) {
             userName = AppiumBy.accessibilityId("Username input field");
+            userNameErrorText = By.xpath("//XCUIElementTypeOther[@name='Username-error-message']/XCUIElementTypeStaticText");
+            passwordErrorText = By.xpath("//XCUIElementTypeOther[@name='Password-error-message']/XCUIElementTypeStaticText");
+            credentialsErrorText = By.xpath("//XCUIElementTypeOther[@name='generic-error-message']/XCUIElementTypeStaticText");
         }
     }
 
@@ -30,12 +39,27 @@ public class LoginPage extends BasePage{
     private WebElement btnLogin;
     MenuPage menupage = new MenuPage();
 
-    public void login(String username, String password){
+    public void login(String username, String password) {
         //menupage.navigateToLogin();
         waitNtype(userName, username);
+        Password.clear();
         Password.sendKeys(password);
         btnLogin.click();
     }
+
+    public String getUserNameErrorText() {
+       return getText(userNameErrorText);
+    }
+
+    public String getPasswordErrorText() {
+        return getText(passwordErrorText);
+    }
+
+    public String getCredentialsErrorText() {
+        return getText(credentialsErrorText);
+    }
+
+
 
 
 
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -4,21 +4,59 @@ import base.BaseTest;
 import driver.AppDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.MenuPage;
 import pages.ProductPage;
 
 public class LoginTest extends BaseTest {
+    MenuPage menupage;
+    LoginPage loginPage;
+    ProductPage prodPage;
+    @BeforeClass
+    public void setup(){
+         menupage = new MenuPage();
+         loginPage = new LoginPage();
+         prodPage = new ProductPage();
+        menupage.navigateToLogin();
+    }
 
-    @Test
+    @Test(priority = 4)
     public void validLoginTest(){
-            MenuPage menupage = new MenuPage();
-            LoginPage loginPage = new LoginPage();
-            ProductPage prodPage = new ProductPage();
-
-            menupage.navigateToLogin();
             loginPage.login("bob@example.com", "10203040");
             Assert.assertTrue(prodPage.waitForProductText());
     }
+
+    @Test(priority = 1)
+    public void invalidLogin_emptyUserNameTest(){
+        loginPage.login("", "10203040");
+        Assert.assertEquals(loginPage.getUserNameErrorText(), "Username is required");
+    }
+
+    @Test(priority = 2)
+    public void invalidLogin_emptyPasswordTest(){
+        loginPage.login("bob@example.com", "");
+        Assert.assertEquals(loginPage.getPasswordErrorText(), "Password is required");
+    }
+
+    @Test(dataProvider = "invalid-login-dataProvider", priority = 3)
+    public void invalidLogin_Test(String uName, String password, String errorText){
+        loginPage.login(uName, password);
+        Assert.assertEquals(loginPage.getCredentialsErrorText(), errorText);
+    }
+
+    @DataProvider(name = "invalid-login-dataProvider")
+    public Object[][] dataProviderArr() {
+        //Create 2 dimensional array
+        //can even take the data from an excel sheet or from a Map
+        //and put it into this array variable
+        Object object[][] = { { "bob@example.com", "1234" ,"Provided credentials do not match any user in this service."},
+                              { "bob@example", "10203040", "Provided credentials do not match any user in this service." },
+        };
+        return object;
+    }
+
+
 }
